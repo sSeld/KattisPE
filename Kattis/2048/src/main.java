ec3866a83a98a331dd504a96808f045bb83c6055
sSeld <jrstephens09@gmail.com>
2018-09-02 15:56:17 -0400

    Updated 2048, cleaner solution. Still not optimal.

--- Kattis/2048/src/main.java
+++ Kattis/2048/src/main.java
@@ -1,404 +1,141 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 
+public class main
+{
+    private static final int LEFT = 0;
+    private static final int UP = 1;
+    private static final int RIGHT = 2;
+    private static final int DOWN = 3;
 
-public class main {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int[][] numGrid = new int[4][4];
-		int gameState = 0;
-		int input;
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int[][] numGrid = new int[4][4];
+        boolean continueGame = true;
+        int input;
 
-		// Input game grid
-		for(int row = 0; row < 4; row++)
-		{
-			for(int col = 0; col < 4; col++)
-			{
+        // Input game grid
+        for (int row = 0; row < 4; row++)
+        {
+            for (int col = 0; col < 4; col++)
+            {
+                numGrid[row][col] = scan.nextInt();
+            }
+        }
 
-				numGrid[row][col] = scan.nextInt();
-			}
-		}
+        //while (continueGame)
+        //{
+            input = scan.nextInt();
+            updateGrid(input, numGrid);
+        //}
+    }
 
-		while(gameState != 2)
-		{
-			input = scan.nextInt();
-			updateGrid(input, numGrid);
-		}
-	}
+    private static void updateGrid(int direction, int[][] numGrid)
+    {
+        switch (direction)
+        {
+            case LEFT:
+                for (int i = 0; i < numGrid.length; i++)
+                    numGrid[i] = calculateRow(numGrid[i]);
+                break;
+            case UP:
+                for (int i = 0; i < numGrid.length; i++)
+                    numGrid = putColumn(numGrid, calculateRow(getColumn(numGrid, i)), i);
+                break;
+            case RIGHT:
+                for (int i = 0; i < numGrid.length; i++)
+                    numGrid[i] = reverse(calculateRow(reverse(numGrid[i])));
+                break;
+            case DOWN:
+                for (int i = 0; i < numGrid.length; i++)
+                    numGrid = putColumn(numGrid, reverse(calculateRow(reverse(getColumn(numGrid, i)))), i);
+                break;
+        }
+        printGrid(numGrid);
+    }
 
-	public static void updateGrid(int direction, int[][] numGrid)
-	{
-		int counter = 0;
-		if(direction == 0)
-		{ //////////// LEFT ////////////
+    private static int[] calculateRow(int[] row)
+    {
+        int[] returnRow = new int[4];
 
-			// Check for zeros and move accordingly
-			for(int row = 0; row <= 3; row++)
-			{
-				for(int col = 0; col < 3; col++)
-				{
-					if(counter >= 3)
-					{
-						counter = 0;
-						break;
-					}
-					if(numGrid[row][col] == 0 && col == 0)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col+1];
-						numGrid[row][col+1] = numGrid[row][col+2];
-						numGrid[row][col+2] = numGrid[row][col+3];
-						numGrid[row][col+3] = 0;
-						counter++;
-						col=0;
-					}
-					else if(numGrid[row][col] == 0 && col == 1)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col+1];
-						numGrid[row][col+1] = numGrid[row][col+2];
-						numGrid[row][col+2] = 0;
-						counter++;
-						col=0;
-					}
-					else if(numGrid[row][col] == 0 && col == 2)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col+1];
-						numGrid[row][col+1] = 0;
-						counter++;
-						col=0;
-					}
-				}
-			}
-			// Check for doubles and add if found
-			for(int row = 0; row <= 3; row++)
-			{
-				for(int col = 0; col < 3; col++)
-				{
-					if(numGrid[row][col] == numGrid[row][col+1])
-					{
-						numGrid[row][col] = numGrid[row][col] + numGrid[row][col+1];
-						numGrid[row][col+1] = 0;
-					}
-				}
-			}
+        int[] rowFiltered = Arrays.stream(row)
+                .filter(i -> i != 0)
+                .toArray();
 
-			// Check for zeros and move accordingly
-			for(int row = 0; row <= 3; row++)
-			{
-				for(int col = 0; col < 3; col++)
-				{
-					if(counter >= 3)
-					{
-						counter = 0;
-						break;
-					}
-					if(numGrid[row][col] == 0 && col == 0)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col+1];
-						numGrid[row][col+1] = numGrid[row][col+2];
-						numGrid[row][col+2] = numGrid[row][col+3];
-						numGrid[row][col+3] = 0;
-						counter++;
-						col=0;
-					}
-					else if(numGrid[row][col] == 0 && col == 1)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col+1];
-						numGrid[row][col+1] = numGrid[row][col+2];
-						numGrid[row][col+2] = 0;
-						counter++;
-						col=0;
-					}
-					else if(numGrid[row][col] == 0 && col == 2)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col+1];
-						numGrid[row][col+1] = 0;
-						counter++;
-						col=0;
-					}
-				}
-			}
-		}
-		else if(direction == 1)
-		{ //////////// UP ////////////
-			// Check for zeros and move accordingly
-			for(int col = 0; col <= 3; col++)
-			{
-				for(int row = 0; row < 3; row++)
-				{
-					if(counter >= 3)
-					{
-						counter = 0;
-						break;
-					}
-					if(numGrid[row][col] == 0 && row == 0)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row+1][col];
-						numGrid[row+1][col] = numGrid[row+2][col];
-						numGrid[row+2][col] = numGrid[row+3][col];
-						numGrid[row+3][col] = 0;
-						counter++;
-						row=0;
-					}
-					else if(numGrid[row][col] == 0 && row == 1)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row+1][col];
-						numGrid[row+1][col] = numGrid[row+2][col];
-						numGrid[row+2][col] = 0;
-						counter++;
-						row=0;
-					}
-					else if(numGrid[row][col] == 0 && row == 2)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row+1][col];
-						numGrid[row+1][col] = 0;
-						counter++;
-						row=0;
-					}
-				}
-			}
-			// Check for doubles and add if found
-			for(int col = 0; col <= 3; col++)
-			{
-				for(int row = 0; row < 3; row++)
-				{
-					if(numGrid[row][col] == numGrid[row+1][col])
-					{
-						numGrid[row][col] = numGrid[row][col] + numGrid[row+1][col];
-						numGrid[row+1][col] = 0;
-					}
-				}
-			}
-			// Check for zeros and move accordingly
-			for(int col = 0; col <= 3; col++)
-			{
-				for(int row = 0; row < 3; row++)
-				{
-					if(counter >= 3)
-					{
-						counter = 0;
-						break;
-					}
-					if(numGrid[row][col] == 0 && row == 0)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row+1][col];
-						numGrid[row+1][col] = numGrid[row+2][col];
-						numGrid[row+2][col] = numGrid[row+3][col];
-						numGrid[row+3][col] = 0;
-						counter++;
-						row=0;
-					}
-					else if(numGrid[row][col] == 0 && row == 1)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row+1][col];
-						numGrid[row+1][col] = numGrid[row+2][col];
-						numGrid[row+2][col] = 0;
-						counter++;
-						row=0;
-					}
-					else if(numGrid[row][col] == 0 && row == 2)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row+1][col];
-						numGrid[row+1][col] = 0;
-						counter++;
-						row=0;
-					}
-				}
-			}
+        for (int pos = 0; pos < rowFiltered.length; pos++)
+        {
+            if ((rowFiltered.length - (pos + 1)) > 0)
+            {
+                // Can check next position
+                if (rowFiltered[pos] == rowFiltered[pos + 1])
+                {
+                    rowFiltered[pos] = rowFiltered[pos] + rowFiltered[pos + 1];
+                    rowFiltered[pos + 1] = 0;
+                }
+            }
+            else
+            {
+                break;
+            }
 
-		}
-		else if(direction == 2)
-		{ //////////// RIGHT ////////////
-			// Check for zeros and move accordingly
-			for(int row = 0; row <= 3; row++)
-			{
-				for(int col = 3; col > 0; col--)
-				{
-					if(counter >= 3)
-					{
-						counter = 0;
-						break;
-					}
-					if(numGrid[row][col] == 0 && col == 3)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col-1];
-						numGrid[row][col-1] = numGrid[row][col-2];
-						numGrid[row][col-2] = numGrid[row][col-3];
-						numGrid[row][col-3] = 0;
-						counter++;
-						col=3;
-					}
-					else if(numGrid[row][col] == 0 && col == 2)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col-1];
-						numGrid[row][col-1] = numGrid[row][col-2];
-						numGrid[row][col-2] = 0;
-						counter++;
-						col=3;
-					}
-					else if(numGrid[row][col] == 0 && col == 1)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col-1];
-						numGrid[row][col-1] = 0;
-						counter++;
-						col=3;
-					}
-				}
-			}
-			// Check for doubles and add if found
-			for(int row = 0; row <= 3; row++)
-			{
-				for(int col = 3; col > 0; col--)
-				{
-					if(numGrid[row][col] == numGrid[row][col-1])
-					{
-						numGrid[row][col] = numGrid[row][col] + numGrid[row][col-1];
-						numGrid[row][col-1] = 0;
-					}
-				}
-			}
+        }
 
-			// Check for zeros and move accordingly
-			for(int row = 0; row <= 3; row++)
-			{
-				for(int col = 3; col > 0; col--)
-				{
-					if(counter >= 3)
-					{
-						counter = 0;
-						break;
-					}
-					if(numGrid[row][col] == 0 && col == 3)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col-1];
-						numGrid[row][col-1] = numGrid[row][col-2];
-						numGrid[row][col-2] = numGrid[row][col-3];
-						numGrid[row][col-3] = 0;
-						counter++;
-						col=3;
-					}
-					else if(numGrid[row][col] == 0 && col == 2)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col-1];
-						numGrid[row][col-1] = numGrid[row][col-2];
-						numGrid[row][col-2] = 0;
-						counter++;
-						col=3;
-					}
-					else if(numGrid[row][col] == 0 && col == 1)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row][col-1];
-						numGrid[row][col-1] = 0;
-						counter++;
-						col=3;
-					}
-				}
-			}
-		}
-		else if(direction == 3)
-		{ //////////// DOWN ////////////
+        rowFiltered = Arrays.stream(rowFiltered)
+                .filter(i -> i != 0)
+                .toArray();
 
-			// Check for zeros and move accordingly
-			for(int col = 0; col <= 3; col++)
-			{
-				for(int row = 3; row > 0; row--)
-				{
-					if(counter >= 3)
-					{
-						counter = 0;
-						break;
-					}
-					if(numGrid[row][col] == 0 && row == 3)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row-1][col];
-						numGrid[row-1][col] = numGrid[row-2][col];
-						numGrid[row-2][col] = numGrid[row-3][col];
-						numGrid[row-3][col] = 0;
-						counter++;
-						row=3;
-					}
-					else if(numGrid[row][col] == 0 && row == 2)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row-1][col];
-						numGrid[row-1][col] = numGrid[row-2][col];
-						numGrid[row-2][col] = 0;
-						counter++;
-						row=3;
-					}
-					else if(numGrid[row][col] == 0 && row == 1)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row-1][col];
-						numGrid[row-1][col] = 0;
-						counter++;
-						row=3;
-					}
-				}
-			}
-			// Check for doubles and add if found
-			for(int col = 0; col <= 3; col++)
-			{
-				for(int row = 3; row > 0; row--)
-				{
-					if(numGrid[row][col] == numGrid[row-1][col])
-					{
-						numGrid[row][col] = numGrid[row][col] + numGrid[row-1][col];
-						numGrid[row-1][col] = 0;
-					}
-				}
-			}
-			// Check for zeros and move accordingly
-			for(int col = 0; col <= 3; col++)
-			{
-				for(int row = 3; row > 0; row--)
-				{
-					if(counter >= 3)
-					{
-						counter = 0;
-						break;
-					}
-					if(numGrid[row][col] == 0 && row == 3)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row-1][col];
-						numGrid[row-1][col] = numGrid[row-2][col];
-						numGrid[row-2][col] = numGrid[row-3][col];
-						numGrid[row-3][col] = 0;
-						counter++;
-						row=3;
-					}
-					else if(numGrid[row][col] == 0 && row == 2)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row-1][col];
-						numGrid[row-1][col] = numGrid[row-2][col];
-						numGrid[row-2][col] = 0;
-						counter++;
-						row=3;
-					}
-					else if(numGrid[row][col] == 0 && row == 1)
-					{ // If next to a zero, swap positions
-						numGrid[row][col] = numGrid[row-1][col];
-						numGrid[row-1][col] = 0;
-						counter++;
-						row=3;
-					}
-				}
-			}
-		}
-		printGrid(numGrid);
-	}
+        //apply zeros
+        for (int i = 0; i < returnRow.length; i++)
+        {
+            returnRow[i] = i < rowFiltered.length ? rowFiltered[i] : 0;
+        }
+        return returnRow;
+    }
 
-	public static void printGrid(int[][] numGrid)
-	{
-		for(int r = 0; r < 4; r++)
-		{
-			for(int c = 0; c < 4; c++)
-			{
-				System.out.print(numGrid[r][c] + " ");
-			}
-			System.out.print("\n");
-		}
-	}
+    private static int[] reverse(int[] row)
+    {
+        for (int i = 0; i < row.length / 2; i++)
+        {
+            int holder = row[i];
+            row[i] = row[row.length - i - 1];
+            row[row.length - i - 1] = holder;
+        }
+        return row;
+    }
 
+    private static int[] getColumn(int[][] numGrid, int col)
+    {
+        int[] returnColumn = new int[4];
+        for (int i = 0; i < numGrid.length; i++)
+        {
+            returnColumn[i] = numGrid[i][col];
+        }
+        return returnColumn;
+    }
+
+    private static int[][] putColumn(int[][] grid, int[] columnArray, int col){
+        for (int i = 0; i < columnArray.length; i++)
+        {
+            grid[i][col] = columnArray[i];
+        }
+        return grid;
+    }
+
+    private static void printGrid(int[][] numGrid)
+    {
+        for (int r = 0; r < 4; r++)
+        {
+            for (int c = 0; c < 4; c++)
+            {
+                System.out.print(numGrid[r][c] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
 }
 
 
